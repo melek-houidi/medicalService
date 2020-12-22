b6a02d8e1fea46559fcc4660664de9275aa19b99
melek-houidi <71493942+melek-houidi@users.noreply.github.com>
2020-12-22 09:52:32 +0100

    login & still working on

--- codex/src/App.js
+++ codex/src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import Login from "./component/Login";
+import Register from "./component/Register";
+import style from "./component/style.css";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="Login">
+          <Login />
+        </div>
+
+        <div className="Register">
+          <Register />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
